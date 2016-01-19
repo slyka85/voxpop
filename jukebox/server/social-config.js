eff6c8d527d2f9e05b27bc753486e5fe86e40c3a
klee0589 <klee0589@gmail.com>
2016-01-19 17:56:15 -0500

    google, twitter, facebook account login

---
+++ jukebox/server/social-config.js
@@ -0,0 +1,9 @@
+ServiceConfiguration.configurations.remove({
+    service: 'facebook'
+});
+ 
+ServiceConfiguration.configurations.insert({
+    service: 'facebook',
+    appId: '1695347174011108',
+    secret: '7493aa401ce8dd9877f059781c7fa35c'
+});
